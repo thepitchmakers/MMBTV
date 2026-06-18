2840f53f773947a1d01928db9001c699d93e5f3b
azizbashar46-dotcom <thepitchmakers@gmail.com>
2026-06-19 03:36:29 +0600

    chore: remove base path configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({ babel: false })],
-  base: '/MMBTV/',   // ← change 'MMBTV' to your exact GitHub repo name
 })
